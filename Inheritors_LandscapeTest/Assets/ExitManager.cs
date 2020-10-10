e020bbd921778ac7951a3183d0234675cc4c5eac
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-10-09 20:36:16 -0400

    ups

---
+++ Inheritors_LandscapeTest/Assets/ExitManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ExitManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+    }
+}
