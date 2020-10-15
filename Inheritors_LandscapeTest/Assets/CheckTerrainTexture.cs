7ea704133f2b943e3d3563c0e489ac1cac7209fd
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-10-14 21:18:17 -0400

    ups

--- Inheritors_LandscapeTest/Assets/CheckTerrainTexture.cs
+++ Inheritors_LandscapeTest/Assets/CheckTerrainTexture.cs
@@ -9,26 +9,33 @@
 
 public class CheckTerrainTexture : MonoBehaviour
 {
+    public bool leavePaths = true;
     public Transform playerTransform;
     public Terrain t;
     public CharacterController characterController;
 
     public int posX;
     public int posZ;
+    int storedPosX = -1;
+    int storedPosZ = -1;
     public float[] textureValues;
+    float[,,] alphaMap;
+    // Keeps track of which coordinates have been walked on.
+    bool[,] walkedMap;
     int numLayers;
-    int trailSize = 3;
+    int trailSize = 1;//3;
     int foremostLayer = 0;
 
     void Start()
     {
         numLayers = t.terrainData.alphamapLayers;
         textureValues = new float[numLayers];
+        InitializeWalkedMap();
     }
 
     void Update()
     {
-        if (characterController.isGrounded)
+        if (characterController.isGrounded && leavePaths)
         {
             ConvertPosition(playerTransform.position);
             CheckTexture();
@@ -38,32 +45,52 @@ public class CheckTerrainTexture : MonoBehaviour
 
     void ChangeTexture()
     {
-        int xSize, zSize;
-        xSize = zSize = trailSize;
-
-        // TODO: make CheckTexture() collect a trailSize x trailSize 3d array to match this function,
-        // so we can compare appropriately.
-        float unchangeableFactor = textureValues[1];
-
-        float[,,] map = new float[xSize, zSize, numLayers];
-        for (int i = 0; i < xSize; i++)
+        // Check that this isn't the position we were just standing in.
+        // TODO: might need to make this as big as the footprint we set and use 2d x,z arrays again.
+        if (posX == storedPosX && posZ == storedPosZ)
         {
-            for (int j = 0; j < zSize; j++)
-            {
-                for (int k = 0; k < numLayers; k++)
-                {
-                    // TODO: find way to mark dirty so this can only happen once to each section.
-                    // May require creating a duplicate array of 1/0 bools same size as terrain splatmap.
-                    // Messing with the API's mark-dirty stuff may fuck up GPU stuff
+            Debug.Log("You've already been here");
+            return;
+        }
 
-                    if (k == 3)
-                        map[i, j, k] = 1f - unchangeableFactor;
-                    else
-                        map[i, j, k] = textureValues[k];
+        storedPosX = posX;
+        storedPosZ = posZ;
+
+        float[,,] remap = new float[trailSize, trailSize, numLayers];
+        for (int i = 0; i < trailSize; i++)
+        {
+            for (int j = 0; j < trailSize; j++)
+            {
+                if (!walkedMap[posX + i, posZ + j])
+                {
+                    for (int k = 0; k < numLayers; k++)
+                    {
+                        // TODO: find way to mark dirty so this can only happen once to each section.
+                        // May require creating a duplicate array of 1/0 bools same size as terrain splatmap.
+                        // Messing with the API's mark-dirty stuff may fuck up GPU stuff\
+
+                        if (k == 3)
+                            remap[i, j, k] = 1f - alphaMap[i, j, 1];
+                        else if (k == 1)
+                        {
+                            remap[i, j, k] = alphaMap[i, j, k];
+                        }
+                        else
+                            remap[i, j, k] = 0f;
+                    }
                 }
+                else
+                {
+                    Debug.Log("You've walked here before");
+                    remap[i, j, 0] = 0f;
+                    remap[i, j, 1] = 0f;
+                    remap[i, j, 2] = 1f;
+                    remap[i, j, 3] = 0f;
+                }
+                walkedMap[posX + i, posZ + j] = true;
             }
         }
-        t.terrainData.SetAlphamaps(posX, posZ, map);
+        t.terrainData.SetAlphamaps(posX, posZ, remap);
 
         int w, h;
         w = h = 12;
@@ -100,16 +127,33 @@ public class CheckTerrainTexture : MonoBehaviour
     // (so we know what we're mainly walking on).
     void CheckTexture()
     {
-        float[,,] alphaMap = t.terrainData.GetAlphamaps(posX, posZ, 1, 1);
+        alphaMap = t.terrainData.GetAlphamaps(posX, posZ, trailSize, trailSize);
         float max = -1f;
-        for (int i = 0; i < numLayers; i++)
+
+        for (int i = 0; i < trailSize; i++)
         {
-            textureValues[i] = alphaMap[0, 0, i];
-            if (textureValues[i] > max)
+            for (int j = 0; j < trailSize; j++)
             {
-                max = textureValues[i];
-                foremostLayer = i;
+                for (int k = 0; k < numLayers; k++)
+                {
+                    textureValues[k] = alphaMap[0, 0, k];
+                    if (textureValues[k] > max)
+                    {
+                        max = textureValues[k];
+                        foremostLayer = k;
+                    }
+                }
             }
         }
+    }
+
+    void InitializeWalkedMap()
+    {
+        int width = t.terrainData.alphamapWidth;
+        int height = t.terrainData.alphamapHeight;
+        walkedMap = new bool[width, height];
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                walkedMap[i, j] = false;
     }
 }
