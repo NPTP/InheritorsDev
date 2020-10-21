7d612670fd6debeb12b005859b46a510f782ca52
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-10-20 20:10:50 -0400

    ups

---
+++ Inheritors_forOct22/Assets/ImportedAssets/GrassPhysics/Scripts/GrassMaterialProfiles/GrassSpecularMaterial.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShadedTechnology.GrassPhysics
+{
+    /// <summary>
+    /// Class stores specular grass material settings
+    /// </summary>
+    [CreateAssetMenu(fileName = "GrassSpecularMaterial", menuName = "GrassPhysics/GrassMaterialProfile/GrassSpecularMaterial")]
+    [GrassMaterial(name = "Specular Material", fileName = "GrassSpecularMaterial", surf = "StandardSpecular", fragmentShader = "GrassSurfaces/GrassSpecularSurface.cginc")]
+    public class GrassSpecularMaterial : GrassMaterialProfile
+    {
+        [LowEndPlatformWarning("Some platforms doesn't support specular materials, so if you getting bad results try changing Grass Material.")]
+        public Color grassTint = Color.white;
+        public Texture glossMap;
+        [Range(0f, 1f)]
+        public float glossStren;
+        public Color specColor = Color.white;
+        [Range(0f, 1f)]
+        public float specStren;
+
+        /// <summary>
+        /// Sets grass material data to global grass shader variables
+        /// </summary>
+        public override void SetMaterialToGrass()
+        {
+            Shader.SetGlobalColor("_GrassColorTint", grassTint);
+            Shader.SetGlobalTexture("_GrassGlossMap", glossMap);
+            Shader.SetGlobalFloat("_GrassGlossStren", glossStren);
+            Shader.SetGlobalColor("_GrassSpecColor", specColor);
+            Shader.SetGlobalFloat("_GrassSpecStren", specStren);
+        }
+    }
+}
---
+++ Inheritors_forOct22/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 4f;
+    private Vector3 forward, right;
+    private CharacterController characterController;
+
+    void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+        characterController = GetComponent<CharacterController>();
+
+        // GameObject recordManager = GameObject.Find("RecordManager");
+        // recordManagerScript = recordManager.GetComponent<RecordManagerScript>();
+        // initialPosition = Vector3.zero;
+        // initialRotation = Quaternion.identity;
+        // moveVectors = new List<Vector3>();
+        // headings = new List<Vector3>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveRight = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 moveForward = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        Vector3 moveVector = moveRight + moveForward;
+        Vector3 heading = Vector3.zero;
+
+        if (moveVector != Vector3.zero)
+        {
+            heading = Vector3.Normalize(moveVector);
+            transform.forward = heading;
+        }
+        moveVector.y += Physics.gravity.y * Time.deltaTime;
+        characterController.Move(moveVector);
+
+        // if (recordManagerScript.recording)
+        // {
+        //     RecordPlayer(moveVector, heading);
+        //     justFinishedRecording = true;
+        // }
+        // else if (justFinishedRecording)
+        // {
+        //     CreatePlaybackGhost();
+        //     moveVectors = new List<Vector3>();
+        //     justFinishedRecording = false;
+        //     initialFrame = true;
+        // }
+    }
+
+    // Recording stuff
+
+    // private RecordManagerScript recordManagerScript;
+    // public GameObject playerGhost;
+    // private List<Vector3> moveVectors;
+    // private List<Vector3> headings;
+    // private Vector3 initialPosition;
+    // private Quaternion initialRotation;
+    // private bool justFinishedRecording = false;
+    // private bool initialFrame = true;
+
+    // void RecordPlayer(Vector3 moveVector, Vector3 heading)
+    // {
+    //     Vector3 moveVectorCopy = new Vector3(moveVector.x, moveVector.y, moveVector.z);
+    //     moveVectors.Add(moveVector);
+    //     headings.Add(heading);
+    //     if (initialFrame)
+    //     {
+    //         initialPosition = transform.position;
+    //         initialRotation = transform.rotation;
+    //         initialFrame = false;
+    //     }
+    // }
+
+    // void CreatePlaybackGhost()
+    // {
+    //     Debug.Log("CreatePlaybackGhost called.");
+    //     GameObject newGhost = Instantiate(playerGhost, initialPosition, initialRotation);
+    //     newGhost.GetComponent<GhostMovement>().PassRecord(moveVectors, headings);
+    // }
+
+}
