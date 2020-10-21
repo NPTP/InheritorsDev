85d7b5644863a3a31ea1db2b70b4ecb8efaedc3c
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-10-20 20:27:10 -0400

    move to move back!

--- Inheritors_forOct22/Assets/ImportedAssets/GrassPhysics/URP/ShaderGraph/Shaders/FullPhysicsFunctions.hlsl
+++
@@ -1,9 +0,0 @@
-﻿#define PHYSICS_FULL 1
-#include "Assets/GrassPhysics/Shaders/GrassPhysics.cginc"
-
-void GrassFullPhysics_float(float3 vertex, out float3 vertOut, out float deformAmount)
-{
-	float4 vert = float4(vertex, 1);
-	deformAmount = GrassFullPhysics(vert);
-	vertOut = vert.xyz;
-}
