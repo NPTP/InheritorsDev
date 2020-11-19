94ccb3e996502a40e6bb5cc5f1cdb36bf9852aee
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-19 17:05:34 -0500

    Add footsteps, toucan, fix audio listener, add colliders

--- Inheritors/Assets/Scripts/Camera/FixedDirAudioListener.cs
+++ Inheritors/Assets/Scripts/Camera/FixedDirAudioListener.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 // Keep the audio listener aligned on the X/Z axis to conform with the camera perspective.
-public class PlayerAudioListener : MonoBehaviour
+public class FixedDirAudioListener : MonoBehaviour
 {
     void LateUpdate()
     {
