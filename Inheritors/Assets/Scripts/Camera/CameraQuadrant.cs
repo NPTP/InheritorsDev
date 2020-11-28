36511bf8f2c328e3bc88e3275d56387153381a31
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-28 01:06:16 -0500

    Finish worldbuilding first major phase

--- Inheritors/Assets/Scripts/Camera/CameraQuadrant.cs
+++ Inheritors/Assets/Scripts/Camera/CameraQuadrant.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraQuadrant : MonoBehaviour
 {
     CameraManager cameraManager;
+    CinemachineVirtualCamera cmVCam = null;
+    bool hasOwnCam = false;
 
     void Start()
     {
         cameraManager = FindObjectOfType<CameraManager>();
+
+        if (transform.childCount > 0)
+            cmVCam = transform.GetChild(0).gameObject.GetComponent<CinemachineVirtualCamera>();
+        if (cmVCam != null)
+            hasOwnCam = true;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        cameraManager.QuadrantCamActivate(this.transform);
+        if (hasOwnCam)
+            cameraManager.SwitchToCam(cmVCam.gameObject.name);
+        else
+            cameraManager.QuadrantCamActivate(this.transform);
     }
 
     private void OnTriggerExit(Collider other)
