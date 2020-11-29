b5382e6839883d1570aaa729f89159ca5a3a171b
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-29 15:03:50 -0500

    Splash screen added

---
+++ Inheritors/Assets/Scripts/UI/Splash/SplashEventCaller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashEventCaller : MonoBehaviour
+{
+    SplashScreen splashScreen;
+
+    void Awake()
+    {
+        splashScreen = FindObjectOfType<SplashScreen>();
+    }
+
+    public void EndOfAnimation()
+    {
+        splashScreen.HandleEndOfAnimation();
+    }
+    
+}
---
+++ Inheritors/Assets/Scripts/UI/Splash/SplashScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    SceneLoader sceneLoader;
+
+    void Awake()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void HandleEndOfAnimation()
+    {
+        sceneLoader.LoadSceneByName("MainMenu");
+    }
+}
