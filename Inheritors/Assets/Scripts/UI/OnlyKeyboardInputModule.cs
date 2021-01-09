3bd1eb2e0ea894343a6d66254d825d0970a18916
Nick Perrin <nick.perrin@mail.utoronto.ca>
2021-01-08 23:16:16 -0500

    Remove mouse processing from main menu event system

---
+++ Inheritors/Assets/Scripts/UI/OnlyKeyboardInputModule.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.EventSystems;
+
+public class OnlyKeyboardInputModule : StandaloneInputModule
+{
+    public override void Process()
+    {
+        bool usedEvent = SendUpdateEventToSelectedObject();
+
+        if (eventSystem.sendNavigationEvents)
+        {
+            if (!usedEvent)
+                usedEvent |= SendMoveEventToSelectedObject();
+
+            if (!usedEvent)
+                SendSubmitEventToSelectedObject();
+        }
+    }
+}
